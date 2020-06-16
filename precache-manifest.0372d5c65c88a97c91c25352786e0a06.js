4c7a9a81c473642fb6c87e89d5089d8566b2d394
morecan <olympspartan@gmail.com>
2020-06-16 21:32:58 +0300

    'master'

---
+++ precache-manifest.0372d5c65c88a97c91c25352786e0a06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "406f3481e924372838320c2ec6975348",
+    "url": "/index.html"
+  },
+  {
+    "revision": "362b2e32d5111e584540",
+    "url": "/static/css/main.9fbb0853.chunk.css"
+  },
+  {
+    "revision": "ca91f596c095bad33321",
+    "url": "/static/js/2.77989d24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.77989d24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "362b2e32d5111e584540",
+    "url": "/static/js/main.accb3fcc.chunk.js"
+  },
+  {
+    "revision": "4305ad129dcf89844c80",
+    "url": "/static/js/runtime-main.3ea18b6e.js"
+  }
+]);
